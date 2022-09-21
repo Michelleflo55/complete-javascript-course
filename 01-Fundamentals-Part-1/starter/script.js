ed53de9ac6df1d704a021fd44c191823018bcdcd
Michelle Flores <lmfg2456@gmail.com>
2022-09-21 12:42:13 -0700

    completed lecture, creating assignments script

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,14 @@
 
 let js = 'amazing';
-if(js=== 'amazing') alert('Javascript is fun')
-
-40 + 8 + 23;
 console.log(40 + 8 + 23)
+
+console.log(' jonas')
+console.log(23)
+
+let fir$tName = ' Michelle' 
+console.log('hello, my name is' + fir$tName + '!')
+
+let myFirstJob = 'hosting'
+let mySecondJob = 'developer'
+
+console.log(myFirstJob + ' was my first job, but now I am a ' + mySecondJob)
