d713206e8b11eaa060be369fdacdba45d1b85005
Michelle Flores <lmfg2456@gmail.com>
2022-09-21 12:46:37 -0700

    completed values and variables lecture

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,5 @@
+let country = 'USA'
+let continent = 'North America'
+let population = '8000'
+
+console.log('I was born in '+ country + ' which is in ' + continent + ' where the population is ' + population + ' people.' )
