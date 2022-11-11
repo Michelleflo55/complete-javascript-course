67bfed380c476ca2e35bb00c51a8d9cf9eb9a733
Michelle Flores <lmfg2456@gmail.com>
2022-11-10 18:36:48 -0800

    completed challenge 1

--- 01-Fundamentals-Part-1/starter/challenges.js
+++ 01-Fundamentals-Part-1/starter/challenges.js
@@ -1,1 +1,21 @@
-console.log('hello')
+
+//CHALLENGE 1
+// //test 1
+// let massMark1 = 78
+// let massJohn1= 92
+// let heightMark1 = 1.69
+// let heightJohn1 = 1.95
+// //test 2
+// let massMark = 95
+// let massJohn = 85
+// let heightMark = 1.88
+// let heightJohn = 1.76
+
+
+// let markBMI = massMark / heightMark **2
+// let johnBMI = massJohn / heightJohn **2
+// console.log(markBMI, johnBMI)
+
+// let markHigherBMI = markBMI > johnBMI
+// console.log(markHigherBMI)
+////////////////////////////////////////////////
