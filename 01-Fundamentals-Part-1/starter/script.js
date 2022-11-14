0d8d1a0156b39a6eea7ef5a0438ac7da84ef32e4
Michelle Flores <lmfg2456@gmail.com>
2022-11-13 19:12:59 -0800

    if /else

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -107,22 +107,44 @@
 
 ////////////////////////////////////////////////
 //STRING & TEMPLATE LITERAL
-    const firstName  = 'Michelle'
-    const job = 'boxer'
-    const birthYear = 1994
-    const year = 2022
+//     const firstName  = 'Michelle'
+//     const job = 'boxer'
+//     const birthYear = 1994
+//     const year = 2022
 
-    const michelle = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!'
-    console.log(michelle)
-        //this template string method below is way cleaner than the one above
-    const michelleNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`
-    console.log(michelleNew)
-console.log(`Just a regular string...`)
-    //creating a string with multiple lines \n\ is new line, was actually a bug
-    console.log('String with \n\
-    multiple \n\
-    ') 
-    // OR you can use backticks to create a string with multiple lines  
-    console.log(`String
-    multiple 
-    lines`)
+//     const michelle = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!'
+//     console.log(michelle)
+//         //this template string method below is way cleaner than the one above
+//     const michelleNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`
+//     console.log(michelleNew)
+// console.log(`Just a regular string...`)
+//     //creating a string with multiple lines \n\ is new line, was actually a bug
+//     console.log('String with \n\
+//     multiple \n\
+//     ') 
+//     // OR you can use backticks to create a string with multiple lines  
+//     console.log(`String
+//     multiple 
+//     lines`)
+
+/////////////////////
+//IF ELSE STATEMENTS
+
+const age = 15
+
+if (age >= 18){
+    console.log('Michelle can start driving license 🥹')
+} else {
+    const yearsLeft = 18-age
+    console.log(`Sorry you have ${yearsLeft} years until you can drive` )
+}
+
+ const bornIn = 2012    
+    let century;
+
+ if(bornIn <= 2000){
+   century = 20
+ } else{
+    century = 21
+ }
+console.log(century)
