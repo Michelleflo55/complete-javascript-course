689217b2b3ac6acbdc20fa6a71e5fe942bcc7f48
Michelle Flores <lmfg2456@gmail.com>
2023-05-01 10:30:51 -0700

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,7 +1,7 @@
   'use strict'
 
   ///////////////////////////////////////
-  /////USE STRICT MODE ABOVE ANY CODE 
+  /////USE STRICT MODE ABOVE ANY CODE
 //   let hasDriversLicense = false
 //   const passTest = true
 
@@ -9,8 +9,22 @@
 //   if (hasDriversLicense) console.log('I can drive')
 
 
-// //const interface = 'Audio' //interface is a reserved word so your console will get an error 
+// //const interface = 'Audio' //interface is a reserved word so your console will get an error
 // //   const if = 23 // unexpected token 'if' will fail because it is a reserved word
+  ///////////////////////////////////////
+  //// FUNCTIONS
+// function logger () {
 
+//   console.log('My name is Michelle')
 
- 
+// }
+// below is CALLING / RUNNING / INVOKING FUNCTION
+// logger()
+
+function fruitProcessor(apples, oranges){
+  console.log(apples, oranges)
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+  return juice
+}
+const appleJuice = fruitProcessor(5,0) //make it appleJuice variable in order to run and save fruitprocessor function 
+console.log(appleJuice)
