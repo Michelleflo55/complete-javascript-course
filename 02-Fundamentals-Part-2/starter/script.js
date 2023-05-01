89a25c1c4842ce722c6cecc4d521eb1d69b93b94
Michelle Flores <lmfg2456@gmail.com>
2023-05-01 09:49:05 -0700

    use strict;

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,16 @@
+  'use strict'
+
+  ///////////////////////////////////////
+  /////USE STRICT MODE ABOVE ANY CODE 
+//   let hasDriversLicense = false
+//   const passTest = true
+
+//   if(passTest) hasDriversLicense = true;
+//   if (hasDriversLicense) console.log('I can drive')
+
+
+// //const interface = 'Audio' //interface is a reserved word so your console will get an error 
+// //   const if = 23 // unexpected token 'if' will fail because it is a reserved word
+
+
+ 
