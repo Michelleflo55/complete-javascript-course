452a8231702f170785e6effa0a6fff36258dc031
Michelle Flores <lmfg2456@gmail.com>
2023-05-02 18:22:25 -0700

    function declarations/expressions

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -1,10 +1,47 @@
 'use strict'
+// FUNCTIONS
+// function describeCountry(country, population, capitalCity){
+//     let description = `${country} has ${population} million people and its capital city is ${capitalCity}`
+//     return description
+// }
 
-function describeCountry(country, population, capitalCity){
-    let description = `${country} has ${population} million people and its capital city is ${capitalCity}`
-    return description
-}
+// let wholeDescription = describeCountry('USA', 'six', 'Washington')
+//  console.log ( wholeDescription)
+//     wholeDescription = describeCountry('Mexico', 'ten', 'df')
+//     console.log(wholeDescription)
+//     wholeDescription = describeCountry('Africa', 'eight', 'nigeria' )
+//     console.log(wholeDescription)
+/// above was function declaration
 
-let wholeDescription = describeCountry('USA', 'six', 'Washington')
- console.log ( wholeDescription)
- 
+///////////////////////////////////////
+//FUNCTION DECLARATIONS VS FUNCTION EXPRESSIONS
+//function declaration 
+//  function percentageOfWorld1(population){
+//     return (population / 7900000000)* 100
+// }
+// //storing populations in variable below = populationPercentage
+//     let populationPercentage = percentageOfWorld1(1441000000)
+//     console.log(populationPercentage)
+    
+//     populationPercentage = percentageOfWorld1(1999999)
+//    console.log(populationPercentage)
+
+//    populationPercentage = percentageOfWorld1(1000000000)
+//     console.log(populationPercentage)
+
+// //function expression variable percentageOfWorld2 stored function
+
+//     const percentageOfWorld2 = function(population ){
+//     return (population / 7900000000)* 100
+//  }
+
+//     let populationPercentage2 = percentageOfWorld2(1441000000)
+//         console.log(populationPercentage2)
+    
+//         populationPercentage2 = percentageOfWorld2(1999999)
+//         console.log(populationPercentage2)
+
+//         populationPercentage2 = percentageOfWorld2(1000000000)
+//         console.log(populationPercentage2)
+
+///////////////////////////////////////
---
+++ 02-Fundamentals-Part-2/starter/script1.js
@@ -0,0 +1,62 @@
+'use strict'
+
+///////////////////////////////////////
+/////USE STRICT MODE ABOVE ANY CODE
+//   let hasDriversLicense = false
+//   const passTest = true
+
+//   if(passTest) hasDriversLicense = true;
+//   if (hasDriversLicense) console.log('I can drive')
+
+
+// //const interface = 'Audio' //interface is a reserved word so your console will get an error
+// //   const if = 23 // unexpected token 'if' will fail because it is a reserved word
+///////////////////////////////////////
+//// FUNCTIONS
+// function logger () {
+
+//   console.log('My name is Michelle')
+
+// }
+// // below is CALLING / RUNNING / INVOKING FUNCTION
+// logger()
+
+// function fruitProcessor(apples, oranges){
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+//   return juice
+// }
+
+// const appleJuice = fruitProcessor(5,0) //make it appleJuice variable in order to run and save fruitprocessor function 
+// console.log(appleJuice) 
+
+// const appleOrangeJuice = fruitProcessor(2,4)
+// console.log(appleOrangeJuice) 
+
+  ///////////////////////////////////////
+
+// FUNCTION DECLARATIONS VS EXPRESSIONS
+
+///BELOW is function declaration
+//function declaration can be called before theyre defined/
+//below age1 is being called before and then calcAge is defined after
+// const age1 = calcAge1(1994)
+// console.log(age1)
+
+
+// function calcAge1(birthYear){
+//     return  2022 - birthYear
+// }
+
+
+
+// //calAge2 will be the variable that is called the funcition
+// // BELOW function expression
+// //functino expression CANNOT be called before it is defined
+
+// const calcAge2 = function (birthYear){
+//     return 2040 - birthYear
+// }
+// const age2 = calcAge2(1994)
+// console.log(age1, age2)
+
+  ///////////////////////////////////////
